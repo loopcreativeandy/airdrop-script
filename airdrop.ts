7add5c8a304ceb1b49994d0dd9b93b84c98d9eb1
loopcreativeandy <loopcreativeandy@gmail.com>
2022-01-24 23:36:04 +0100

    post video state
    tmp commit

--- airdrop.ts
+++ airdrop.ts
@@ -2,6 +2,7 @@
 import * as web3 from '@solana/web3.js';
 import * as anchor from "@project-serum/anchor";
 import { assert } from 'console';
+import * as splToken from "@solana/spl-token";
 
 async function getOwnerForNFT(c: web3.Connection, tokenMintString: string) : Promise<web3.PublicKey>{
 
@@ -21,14 +22,39 @@ async function getOwnerForNFT(c: web3.Connection, tokenMintString: string) : Pro
     return owner;
 }
 
+async function getAllNFTsForCreator(c: web3.Connection, verifiedCreator: string){
+
+    const config : web3.GetProgramAccountsConfig = {
+        commitment: undefined,
+        encoding: "base64",
+        dataSlice: undefined,
+        filters: [
+            {
+                "dataSize":
+                "memcmp": {
+                    "offset": 0,
+                    "bytes": verifiedCreator
+                }
+            }
+        ]
+    }
+    const accountList = await c.getProgramAccounts(splToken.TOKEN_PROGRAM_ID, config);
+
+    console.log(accountList);
+    
+
+}
+
 async function main(){
     
     
     const rpcHost = "https://api.mainnet-beta.solana.com"
     const c = new anchor.web3.Connection(rpcHost);
 
-    const owner = await getOwnerForNFT(c, "AP7VntKBj4253RV6ktMrZJst5JFFmrQnHy29HC7rHvd");
-    console.log(owner.toBase58());
+    //const owner = await getOwnerForNFT(c, "AP7VntKBj4253RV6ktMrZJst5JFFmrQnHy29HC7rHvd");
+    //console.log(owner.toBase58());
+
+    await getAllNFTsForCreator(c, "2wtqpaZZArqYaja55wJ3Wp6gtitWAncG1FwabGV2yNwF");
 }
 
 main();
